c6b4e303b1e9fe1e76267e56a851e53751cb6429
matpazikas <logonrmlocal@fiap.com.br>
2024-04-04 21:02:29 -0300

    Prova Finalizada!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MeuComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'checkpoint1';
+  mensagem:string = "Olá mundo!"
 }
